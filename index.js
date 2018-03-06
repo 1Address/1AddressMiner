071a2dfacd988646a27bd6ce2d0fd46767a07f70
Anton Bukov <k06aaa@gmail.com>
2018-03-07 00:47:10 +0300

    Improved a lot for mining

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const contractAddress = '0x16b0e794fEb8C4008C62269134908b12C6Ed5750';
 const gasToContract = 1200000;
 const abi = [{"constant":true,"inputs":[{"name":"privateKey","type":"uint256"}],"name":"bitcoinPublicKey","outputs":[{"name":"","type":"uint256"},{"name":"","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"p","type":"uint256"}],"name":"invmod","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":false,"inputs":[],"name":"startUpgrade","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"endUpgrade","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"bytes"},{"name":"b","type":"bytes"}],"name":"equalBytesToBytes","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[{"name":"publicXPoint","type":"uint256"},{"name":"publicYPoint","type":"uint256"}],"name":"createBtcAddress","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[{"name":"_value","type":"uint256"},{"name":"appCode","type":"bytes1"}],"name":"toBase58Checked","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[{"name":"prefix","type":"bytes"},{"name":"length","type":"uint256"}],"name":"complexityForBtcAddressPrefixWithLength","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"},{"name":"p","type":"uint256"}],"name":"submod","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"completedTasks","outputs":[{"name":"taskType","type":"uint8"},{"name":"taskId","type":"uint256"},{"name":"creator","type":"address"},{"name":"reward","type":"uint256"},{"name":"data","type":"bytes32"},{"name":"dataLength","type":"uint256"},{"name":"requestPublicXPoint","type":"uint256"},{"name":"requestPublicYPoint","type":"uint256"},{"name":"answerPrivateKey","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"taskId","type":"uint256"},{"name":"answerPrivateKey","type":"uint256"}],"name":"solveTask","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"prefix","type":"bytes"}],"name":"complexityForBtcAddressPrefix","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[{"name":"taskId","type":"uint256"}],"name":"safeIndexOfTaskId","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"bytes"},{"name":"b","type":"bytes"}],"name":"lengthOfCommonPrefix","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[],"name":"totalReward","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"completedTasksCount","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"taskId","type":"uint256"},{"name":"reward","type":"uint256"}],"name":"payForTask","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"prefix","type":"bytes"},{"name":"length","type":"uint256"}],"name":"countBtcAddressLeadingOnes","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"tasks","outputs":[{"name":"taskType","type":"uint8"},{"name":"taskId","type":"uint256"},{"name":"creator","type":"address"},{"name":"reward","type":"uint256"},{"name":"data","type":"bytes32"},{"name":"dataLength","type":"uint256"},{"name":"requestPublicXPoint","type":"uint256"},{"name":"requestPublicYPoint","type":"uint256"},{"name":"answerPrivateKey","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"upgradableState","outputs":[{"name":"isUpgrading","type":"bool"},{"name":"prevVersion","type":"address"},{"name":"nextVersion","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_token","type":"address"},{"name":"loser","type":"address"}],"name":"recoverLost","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"prefixArg","type":"bytes"}],"name":"requireValidBicoinAddressPrefix","outputs":[],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[],"name":"tasksCount","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"source","type":"bytes"}],"name":"bytesToBytes32","outputs":[{"name":"result","type":"bytes32"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[{"name":"publicXPoint","type":"uint256"},{"name":"publicYPoint","type":"uint256"}],"name":"createBtcAddressHex","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[{"name":"x1","type":"uint256"},{"name":"y1","type":"uint256"},{"name":"x2","type":"uint256"},{"name":"y2","type":"uint256"}],"name":"addXY","outputs":[{"name":"x3","type":"uint256"},{"name":"y3","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[{"name":"x1","type":"uint256"},{"name":"y1","type":"uint256"},{"name":"privateKey","type":"uint256"}],"name":"mulXY","outputs":[{"name":"x2","type":"uint256"},{"name":"y2","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"bytes32"},{"name":"b","type":"bytes32"}],"name":"lengthOfCommonPrefix3232","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"indexOfTaskId","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"prefix","type":"bytes"},{"name":"reward","type":"uint256"},{"name":"requestPublicXPoint","type":"uint256"},{"name":"requestPublicYPoint","type":"uint256"}],"name":"createBitcoinAddressPrefixTask","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"x1","type":"uint256"},{"name":"y1","type":"uint256"}],"name":"doubleXY","outputs":[{"name":"x2","type":"uint256"},{"name":"y2","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"bytes32"},{"name":"b","type":"bytes"}],"name":"lengthOfCommonPrefix32","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"bytes32"},{"name":"b","type":"bytes"}],"name":"equalBytes32ToBytes","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[],"name":"ec","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"token","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"nextTaskId","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"_ec","type":"address"},{"name":"_token","type":"address"},{"name":"_prevVersion","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"taskId","type":"uint256"}],"name":"TaskCreated","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"taskId","type":"uint256"}],"name":"TaskSolved","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"prevVersion","type":"address"}],"name":"Initialized","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"nextVersion","type":"address"}],"name":"Upgrading","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"nextVersion","type":"address"}],"name":"Upgraded","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"previousOwner","type":"address"},{"indexed":true,"name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}];
 
-const { exec } = require('child_process');
+const { exec, spawn } = require('child_process');
 const bs58 = require('bs58');
 const Web3 = require('web3');
 const Tx = require('ethereumjs-tx');
@@ -52,6 +52,11 @@ a7459afcefe4a4d355309788396eb13cb79137f80527a03b5caad8c503c85dfb
 4b472ef03622f504f907c5c42bd1549fe895e1d0bbbd6a2d0ca6fd72fdc25500
 */
 
+process.on('unhandledRejection', (reason, p) => {
+    console.log('Unhandled Rejection at: Promise', p, '\nreason:', reason, '\nstack:', reason.stack);
+    // application specific logging, throwing an error, or other logic here
+});
+
 (async function() {
     // Accessing contract
     const web3 = new Web3(nodeUrl.startsWith('ws') ? new Web3.providers.WebsocketProvider(nodeUrl) : new Web3.providers.HttpProvider(nodeUrl));
@@ -60,11 +65,6 @@ a7459afcefe4a4d355309788396eb13cb79137f80527a03b5caad8c503c85dfb
         return;
     }
     var contract = new web3.eth.Contract(abi, contractAddress);
-    var nextVersion = 0;
-    while ((nextVersion = (await contract.methods.upgradableState().call()).nextVersion) != 0) {
-        contract = new web3.eth.Contract(abi, nextVersion);
-        console.log('Upgraded contract to next version: ' + nextVersion);
-    }
 
     // Accessing account
     if (minerPrivateKey.length != 66) {
@@ -78,33 +78,76 @@ a7459afcefe4a4d355309788396eb13cb79137f80527a03b5caad8c503c85dfb
     var nonce = await web3.eth.getTransactionCount(minerAccount.address);
     console.log('Nonce = ' + nonce);
 
+    var proc = null;
+    var subscription = null;
+    var transactionPromise = null;
+
     while (true) {
+
+        // Move to the latest contract
+        var nextVersion = 0;
+        while ((nextVersion = (await contract.methods.upgradableState().call()).nextVersion) != 0) {
+            contract = new web3.eth.Contract(abi, nextVersion);
+            console.log('Upgraded contract to next version: ' + contract.options.address);
+        }
+
+        if ((await contract.methods.upgradableState().call()).isUpgrading) {
+            console.log('Waiting until contract upgrade finished');
+            await new Promise(done => setTimeout(done, 5000));
+            continue;
+        }
+
+        // Resubscribe
+        if (subscription != null) {
+            subscription.unsubscribe(function(error, success) {
+                if (error) {
+                    console.log('Error while unsubscribing: ' + error);
+                }
+            });
+        }
+        subscription = web3.eth.subscribe('logs', {
+            address: contract.address,
+            topics: [
+                web3.utils.sha3('TaskCreated(uint256)'),
+                web3.utils.sha3('TaskSolved(uint256)'),
+                web3.utils.sha3('Upgraded(address)'),
+            ]
+        }, function(error, result) {
+            if (!error) {
+                console.log(result);
+            }
+            proc.kill('SIGINT');
+        });
 
         // Get all tasks
         const tasksCount = await contract.methods.tasksCount().call();
         console.log('Found ' + tasksCount + ' tasks:');
-
         const tasks = [];
         for (var i = 0; i < tasksCount; i++) {
             const task = await contract.methods.tasks(i).call();
             task.difficulty = await contract.methods.complexityForBtcAddressPrefixWithLength(task.data, task.dataLength).call();
             task.prefix = web3.utils.hexToAscii(task.data);
             tasks.push(task);
-            console.log('Task #' + i + ': ' + task.data + ' (' +
+            console.log('Task #' + i + ': ' + web3.utils.toAscii(task.data) + ' (' +
                         task.reward/10**18 + ' VIP, ' +
                         task.difficulty/10**6 + ' MH, ' +
                         task.reward/task.difficulty/10**12 + ' VIP/MH)');
         }
         tasks.sort(function(a, b){ a.reward/a.difficulty - b.reward/b.difficulty });
 
+        if (tasks.length == 0) {
+            await new Promise(done => setTimeout(done, 5000));
+            continue;
+        }
+
         const task = tasks[0];
         const publicKey = `04${web3.utils.toHex(task.requestPublicXPoint).substr(2)}${web3.utils.toHex(task.requestPublicYPoint).substr(2)}`;
         const cmd = `${vanitygenCmd} -P ${publicKey} -Z ${minerAccount.address.substr(2,32)} ${task.prefix}`;
         console.log(cmd);
-        exec(cmd, {cwd: vanitygenDir}, async function(error, stdout, stderr) {
-            if (error) {
-                console.error(`exec error: ${error}`);
-                return;
+        transactionPromise = new Promise(resolve => proc = exec(cmd, {cwd: vanitygenDir, detached: true}, async function(e, stdout, stderr) {
+            if (e instanceof Error) {
+                console.error(e);
+                throw e;
             }
 
             const privkeyPart = stdout.split('\n').find(line => line.startsWith('PrivkeyPart:')).split(' ')[1];
@@ -116,7 +159,7 @@ a7459afcefe4a4d355309788396eb13cb79137f80527a03b5caad8c503c85dfb
                 const data = contract.methods.solveTask(task.taskId, privkeyPartHex).encodeABI();
                 var options = {
                     from: minerAccount.address,
-                    to: contractAddress,
+                    to: contract.options.address,
                     data: data,
                     gas: gasToContract,
                     gasPrice: gasPrice,
@@ -127,19 +170,30 @@ a7459afcefe4a4d355309788396eb13cb79137f80527a03b5caad8c503c85dfb
                 var serializedTx = tx.serialize();
 
                 web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
-                .on('transactionHash', function(hash){
-                    console.log(`Sending transaction ${explorerUrl}${hash}`);
-                })
-                .on('receipt', function(receipt){
-                    console.log(`Transaction was mined.`);
-                })
-                .on('error', console.error);
+                    .on('transactionHash', function(hash) {
+                        console.log(`Sending transaction ${explorerUrl}${hash}`);
+                    })
+                    .on('error', function(error) {
+                        console.error(error);
+                        resolve();
+                    })
+                    .then(function(receipt) {
+                        console.log(`Transaction was mined.`);
+                        nonce++;
+                        resolve();
+                    });
             } else {
-                console.log('Task was already solved');
+                console.log('Task was already solved by someone');
             }
-        });
+        }));
 
-        return; // TODO: rm
+        // Await process termination
+        await new Promise(done => proc.on('close', async function(code) {
+            if (code == 0) {
+                await transactionPromise;
+            }
+            done();
+        }));
     }
 
 })();
